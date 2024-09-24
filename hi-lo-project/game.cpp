e806b68eece80a2bfff767491b39210ee0832ad2
Ali-Tharwat8 <alitharwat128@gmail.com>
2024-09-24 09:23:02 +0300

    playHiLo

--- hi-lo-project/game.cpp
+++ hi-lo-project/game.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
 int getGuess(int count);
+int guessed_num;
 
-bool playHiLo(int number) {
-
+bool playHiLo(int number) 
+{
+	for (int i = 1; i < 8; i++)
+	{
+		guessed_num = getGuess(i);
+		if (guessed_num == number)
+		{
+			std::cout << "Correct! You win!\n";
+			return true;
+		}
+		if (guessed_num > number)
+			std::cout << "Your guess is too high\n";
+		if (guessed_num < number)
+			std::cout << "Your guess is too low\n";
+	}
+	std::cout << "Sorry, you used all 7 guesses :(. You lose!\n ";
 	return false;
 }
