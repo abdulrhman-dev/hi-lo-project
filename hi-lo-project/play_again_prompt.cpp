76f4302306712d12160b0f47e2f933341d769970
abdulrhman-dev <abdulrhman.jalal.dev@gmail.com>
2024-09-25 07:34:59 +0300

    Created the playAgainFunction

--- hi-lo-project/play_again_prompt.cpp
+++ hi-lo-project/play_again_prompt.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
-
 bool playAgain() {
+	while (true) {
+		std::cout << "Do you want to try again (y/n)? ";
+		char answer{};
+		std::cin >> answer;
+
+		bool success{ std::cin };
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cin.clear();
+
+		if (!success)
+			continue;
+
+		if (answer == 'y')
+			return true;
+		else if (answer == 'n')
+			return false;
+	}
 
 	return false;
 }
