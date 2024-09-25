4a5519ff428fc5aa2054a970dff82aca72d05bb7
Hassan Eledel <hassaneledel5@gmail.com>
2024-09-25 06:37:19 +0300

    guess_input(add 100 to the numbers)

--- hi-lo-project/guess_input.cpp
+++ hi-lo-project/guess_input.cpp
@@ -7,11 +7,11 @@ int getGuess(int count)
 	{
 		std::cout << "Guess \"" << count << "\" is : ";
 		std::cin >> guess;
-		if (!(guess > 0 && guess < 100))
+		if (!(guess > 0 && guess <= 100))
 		{
 			std::cout << "The number is between 1 and 100 \n";
 		}
-	} while (!(guess > 0 && guess < 100));
+	} while (!(guess > 0 && guess <= 100));
 
 	return guess;
 }
