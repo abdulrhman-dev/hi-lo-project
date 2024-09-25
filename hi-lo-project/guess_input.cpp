c8896e6b599d721bbecf543660c116a8a599f6d7
abdulrhman-dev <abdulrhman.jalal.dev@gmail.com>
2024-09-25 14:49:12 +0300

    Added input validation to getGuess()

--- hi-lo-project/guess_input.cpp
+++ hi-lo-project/guess_input.cpp
@@ -7,6 +7,17 @@ int getGuess(int count)
 	{
 		std::cout << "Guess \"" << count << "\" is : ";
 		std::cin >> guess;
+
+		bool success{ std::cin };
+
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+		if (!success) {
+			std::cout << "Recevied invalid input please try again.\n";
+			continue;
+		}
+
 		if (!(guess > 0 && guess <= 100))
 		{
 			std::cout << "The number is between 1 and 100 \n";
